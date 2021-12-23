4fa8dc3b5c76eccafd9f1846f35262238f64a3be
Tech Head <techhead82@gmail.com>
2021-12-23 15:37:58 -0600

    Updated file to remove Ubuntu specific functions

--- Update.sh
+++ Update.sh
@@ -11,7 +11,8 @@ apt-get -y upgrade &&
 echo "Checking for Distro Upgrades and Installing"
 apt-get -y dist-upgrade &&
 echo "Checking for OS Upgrades and Installing"
-apt-get install update-manager-core &&
+# The next line is Ubuntu specific
+# apt-get install update-manager-core &&
 do-release-upgrade &&
 echo "Checking for and Removing Unneeded Programs"
 apt-get -y autoremove && apt-get -y autoclean && apt-get -y clean &&
