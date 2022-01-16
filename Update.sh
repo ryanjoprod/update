8ed0563c3aac1cb27fa07c63eb4ceff92ba63086
Tech Head <techhead82@gmail.com>
2022-01-16 09:43:36 -0600

    Fixed issue with colors not displaying properly in commandline.

--- Update.sh
+++ Update.sh
@@ -4,16 +4,16 @@
 # auto OS updater
 ####
 
-echo "\033[01;36mChecking for Updates\033[00;37m"
+echo "\e[01;36mChecking for Updates\e[m"
 apt-get update &&
-echo "\033[01;36mStarting Updates\033[00;37m"
+echo "\e[01;36mStarting Updates\e[m"
 apt-get -y upgrade &&
-echo "\033[01;36mChecking for Distro Upgrades and Installing\033[00;37m"
+echo "\e[01;36mChecking for Distro Upgrades and Installing\e[m"
 apt-get -y dist-upgrade &&
-echo "\033[01;36mChecking for OS Upgrades and Installing\033[00;37m"
+echo "\e[01;36mChecking for OS Upgrades and Installing\e[m"
 # The next two lines are Ubuntu specific
 # apt-get install update-manager-core &&
 # do-release-upgrade &&
-echo "\033[01;36mChecking for and Removing Unneeded Programs\033[00;37m"
+echo "\e[01;36mChecking for and Removing Unneeded Programs\e[m"
 apt-get -y autoremove && apt-get -y autoclean && apt-get -y clean &&
-echo "\033[01;36mUpgrades Finished"
+echo "\e[01;36mUpgrades Finished"
